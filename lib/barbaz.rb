70c6080b7c14a2e2afe555add0bff132a4a8b5d9
benk-gc <bkyriakou@gocardless.com>
2024-07-11 11:30:25 +0100

    Initial version

---
+++ lib/barbaz.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "barbaz/version"
+
+module Barbaz
+  def self.hello
+    puts "world"
+  end
+end
---
+++ lib/barbaz/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Barbaz
+  VERSION = "0.1.0"
+end
